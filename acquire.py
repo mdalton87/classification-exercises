71e597e29df74404f8904bd9fe9f99bdd1ca4fb5
Matthew Dalton <matthew.w.dalton@gmail.com>
2021-02-23 09:11:11 -0600

    building acquire.py continuing classification exercises, and updated gitignore

--- acquire.py
+++ acquire.py
@@ -1,4 +1,17 @@
+import pandas as pd
+import numpy as np
+import os
 
+# visualize
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.rc('figure', figsize=(11, 9))
+plt.rc('font', size=13)
+
+# acquire
+from env import host, user, password
+from pydataset import data
+import acquire
 
 # data = sns.load_dataset('iris')
 # df_iris = pd.DataFrame(data)
@@ -51,7 +64,6 @@
 # df_google.Embarked.unique()
 
 
-
 def get_connection(db, user=user, host=host, password=password):
     '''
     This function uses my info from my env file to
@@ -60,6 +72,8 @@ def get_connection(db, user=user, host=host, password=password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'    
        
 
+## TITANIC DATABASE        
+        
 def new_titanic_data():
     '''
     This function reads in the titanic data from the Codeup db
@@ -89,3 +103,26 @@ def get_titanic_data(cached=False):
         df = pd.read_csv('titanic_df.csv', index_col=0)
         
     return df
+
+## IRIS DATABASE
+
+def get_iris_data(cached=False):
+    '''
+    This function reads in iris data from Codeup database and writes data to
+    a csv file if cached == False or if cached == True reads in titanic df from
+    a csv file, returns df.
+    '''
+    if cached == False or os.path.isfile('iris_df.csv') == False:
+        
+        # Read fresh data from db into a DataFrame.
+        df = sns.load_dataset('iris')
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('iris_df.csv')
+        
+    else:
+        
+        # If csv file exists or cached == True, read in data from csv.
+        df = pd.read_csv('iris_df.csv', index_col=0)
+        
+    return df    
